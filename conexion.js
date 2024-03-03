855eab81602422042d2f2d55620b00cf8d806ffd
BraulioUP <22050149@alumno.utmetropolitana.edu.mx>
2024-03-02 18:47:55 -0600

    Actualiza el manejo de errores y agrega mensajes de registro

--- conexion.js
+++ conexion.js
@@ -19,26 +19,36 @@ var config = {
 
 const connection = new Connection(config);
 
-connection.connect();
-
 connection.on("connect", (err) => {
   if (err) {
-    console.log("error al conectarse a la base de datos");
-    throw err;
+    console.error("Error al conectarse a la base de datos:", err);
+  } else {
+    console.log("Conexión establecida con éxito.");
+    executeStatement();
   }
-  executeStatement();
+});
+
+connection.on("error", (err) => {
+  console.error("Error de conexión:", err);
 });
 
 function executeStatement() {
-  const request = new Request("SELECT 24/2", (err, rowCont) => {
+  const request = new Request("SELECT 24/2", (err, rowCount) => {
     if (err) {
-      throw err;
+      console.error("Error al ejecutar la consulta:", err);
+    } else {
+      console.log(`Consulta ejecutada con éxito. Número de filas: ${rowCount}`);
     }
     connection.close();
   });
-  request.on("row", (columms) => {
-    console.log(columms);
+
+  request.on("row", (columns) => {
+    columns.forEach((column) => {
+      console.log(`${column.metadata.colName}: ${column.value}`);
+    });
   });
 
   connection.execSql(request);
 }
+
+connection.connect();
